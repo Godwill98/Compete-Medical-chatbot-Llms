cf8f033d6574c555b40017df3cb579177cebda72
Godwill Kiplagat <godwillkiprop@gmail.com>
2025-09-04 11:12:35 +0300

    created the folder structure

---
+++ template.sh
@@ -0,0 +1,16 @@
+#create project directories
+mkdir -p src
+mkdir -p research
+
+#create files
+touch src/__init__.py
+touch src/helper.py
+touch src/promptpy
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+touch README.md
+
+echo "Project structure created successfully."
